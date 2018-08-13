242cc8398d52a5c8af7c0b400459a2d6365bdfa9
Ataberk Dinc <ataberkdinc@mail.com>
2018-08-13 15:43:07 +0300

    initial commit

---
+++ app/src/main/java/com/ataberkdinc/daksilbilisimwebview/MainActivity.java
@@ -0,0 +1,41 @@
+package com.ataberkdinc.daksilbilisimwebview;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webview = (WebView) findViewById(R.id.webview);
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.loadUrl("http://daksilbilisim.wordpress.com");
+
+        final ProgressDialog progress = ProgressDialog.show(this, "Daksil Bilisim", "Yükleniyor....", true);
+        progress.show();
+        webview.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                Toast.makeText(getApplicationContext(), "Sayfa yüklendi", Toast.LENGTH_SHORT).show();
+                progress.dismiss();
+            }
+
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                Toast.makeText(getApplicationContext(), "Bir hata oluştu", Toast.LENGTH_SHORT).show();
+                progress.dismiss();
+            }
+        });
+
+    }
+
+}
